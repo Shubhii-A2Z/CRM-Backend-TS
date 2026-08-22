727cb91bd15de48d348c1fa773a4380495da93f6
unknown <negishubham2005@gmail.com>
2026-08-22 12:29:48 +0530

    add code changes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import { genericErrorHandler } from './middlewares/error.middleware';
 import serverConfig from './config/server.config';
 import logger from './config/logger.config';
-import { prismaClient } from './prisma/client';
 
 const app=express();
 
@@ -16,13 +15,5 @@ app.use(genericErrorHandler);
 
 app.listen(serverConfig.PORT,async()=>{
     console.log(`Server started at PORT: ${serverConfig.PORT}`);
-    const user=await prismaClient.user.create({
-        data:{
-            name: "prisma",
-            email: "hello@prisma",
-            password: "12345"
-        }
-    });
-    console.log(user);
     logger.info('Server Started',{success: true}); // Logging with Metadata
 });
